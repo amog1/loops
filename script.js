5c09b4f0f544d506f4ec3544e0583a9eeaca5843
amog1 <160067063+amog1@users.noreply.github.com>
2024-05-17 13:56:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+// console.log("Lets go oilers!");
+// console.log("Lets go oilers!");
+// console.log("Lets go oilers!");
+// console.log("Lets go oilers!");
+// console.log("Lets go oilers!");
+
+//
+// let i = 1; // control variable -- tells the loop to break
+
+// while (i <= 10) { // conditional -- it breaks
+//   console.log("Lets go oilers!");
+//   console.log("Lets go oilers!");
+//   i++; // iterator -- stops the loop
+// }
+
+// Example. Write a while loop that prints the phrase "Hello, world!" 25 times.
+// let i = 1;
+// while (i <= 25) {
+//   console.log("Hello world ");
+//   console.log(i);
+//   i++;
+// }
+
+//
+// let a = 0;
+// while (a < 100) {
+//   console.log(a);
+//   a++;
+// }
+
+// console.log("loop finish");
+
+// Print all ODD numbers from 100 to 130
+// var x = 100;
+// while (x <= 130) {
+//   //check if x is Odd
+//   if (x % 2 == 1) console.log(x);
+//   x++;
+// } // end of the loop
+
+// var x = 101;
+// while (x <= 129) {
+//   console.log(x);
+//   x += 2;
+// }
+
+// ADD all numbers  from 1 to 10
+// 1+2+3+4+..... + 10=______let
+// let i = 1;
+// let total = 0;
+// while (i <= 10) {
+//   total = total + i;
+//   console.log(`i ; ${i}`);
+//   console.log(`toatal ;${total}`);
+//   i++;
+// }
+
+// Que1
+let a = 1;
+while (a <= 800) {
+  console.log("hello ");
+  a++;
+}
+// Que2
+let b = 20;
+while (b <= 500) {
+  if (b % 5 == 0) console.log(b);
+
+  b++;
+}
+
+// Que3
+let c = 10;
+while (c <= 100) {
+  if (c % 2 == 0) console.log(c);
+  c++;
+}
+// Que4
